699fc5a56f867816601ead54b493e0d1f4c734a9
Phats Lys <phatly2001@gmail.com>
2021-05-22 08:52:03 +0700

    UI version 1.0

--- SecurityService/wwwroot/js/site.js
+++ SecurityService/wwwroot/js/site.js
@@ -1,4 +1,158 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+
 // Write your JavaScript code.
+
+var swiper = new Swiper('.swiper-container1', {
+    direction: 'vertical',
+    spaceBetween: 5,
+    effect: 'fade',
+    centeredSlides: true,
+    
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination1',
+        clickable: true,
+    },
+    
+});
+
+var mySwiper = new Swiper(".swiper-container2", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    mousewheel: true,
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination2',
+        clickable: true,
+    },
+    
+});
+
+var swiper3 = new Swiper('.swiper-container3', {
+    slidesPerView: 3,
+    autoplay: {
+        delay: 300000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    
+   
+});
+
+
+
+
+
+// search function
+
+$(document).ready(function () {
+    var myNav = document.getElementById("myNav");
+    var searchAround = document.getElementById("searchAround");
+
+    $('#search').click(function () {
+        $('.myNav-item').addClass('hide-item');
+        $('.search-form').addClass('active');
+        $('.close').addClass('active');
+        $('#search').hide();
+        $('.search-dropdown').addClass('active');
+        myNav.style.backgroundColor = "rgba(0,0,0,1)";
+        searchAround.style.display = "block";
+        document.body.style.overflow = "hidden";
+        
+        
+        
+    })
+    $('.close').click(function () {
+        $('.myNav-item').removeClass('hide-item');
+        $('.search-form').removeClass('active');
+        $('.close').removeClass('active');
+        $('.search-dropdown').removeClass('active');
+        $('#search').show();
+        myNav.style.removeProperty("background-color");
+        searchAround.style.display = "none";
+        document.body.style.removeProperty("overflow");
+        
+    })
+})
+
+
+// navigation sticky
+
+window.onscroll = function () { myFunction() };
+
+var header = document.getElementById("myNav");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+      
+    } else {
+        header.classList.remove("sticky");
+
+    }
+};
+
+
+//hamberger menu
+
+$(document).ready(function () {
+    var myNav_list = document.getElementById('myNav-list');
+    var myNav = document.getElementById('myNav');
+    var navIcon = document.getElementById('nav-icon');
+    $('#menu-btn').click(function () {
+        if ($(this).prop("checked") == true) {
+            myNav_list.classList.add("active");
+            myNav.classList.add("active");
+            document.body.style.overflow = "hidden";
+            navIcon.classList.add("active");
+        } else {
+            myNav_list.classList.remove("active");
+            myNav.classList.remove("active");
+            document.body.style.removeProperty("overflow");
+            navIcon.classList.remove("active");
+
+        }
+    })
+})
+
+// fade in when sroll down
+
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fade").each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      
+      /* If the element is completely within bounds of the window, fade it in */
+        if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+
+        if ($(this).css("opacity")==0) {$(this).fadeTo(0.5,1);}
+        }
+       // else { //object goes out of view (scrolling up)
+      //  if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+      //}
+    });
+  }).scroll(); //invoke scroll-handler on page-load
+});
